df627212ef51b5e5a6249cde4569ec1f542c25cd
Mr1-D3CRYPT <ashishthomas06@gmail.com>
2024-03-24 11:06:12 +0530

    Something

--- markapp/urls.py
+++ markapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from  django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index),
@@ -11,3 +13,7 @@ urlpatterns = [
     path('login_view',views.login_view),
     path('logout_view',views.logout_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)        
